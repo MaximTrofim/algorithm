4e496cc41085d045d729ac5301c8ad4d1efe2e58
MaximTrofim <oklickk@yandex.ru>
2019-08-23 23:38:36 +0300

    добавил lesson 5

---
+++ src/main/java/dz_lesson_5/Pow.java
@@ -0,0 +1,34 @@
+package dz_lesson_5;
+
+public class Pow {
+    public static void main(String[] args) {
+        //System.out.println(pow_cyclic(2,8));
+        System.out.println(pow_recursive(2,-2));
+    }
+    public static double pow_cyclic(double a, double b){
+        double result = 1;
+        if (b > 0){
+            for (int i = 0; i < b ; i++) {
+                result = result * a;
+            }
+            return result;
+        }
+        if (b < 0){
+            for (int i = 0; i > b; i--) {
+                result = result * a;
+            }
+            return 1 / result;
+        }
+        return 1;
+    }
+
+    public static double pow_recursive(double a, double b){
+        if (b > 0){
+            return a * pow_recursive(a,b - 1);
+        }
+        if (b < 0){
+            return 1 / (a * pow_recursive(a,Math.abs(b) - 1));
+        }
+        return 1;
+    }
+}
---
+++ src/main/java/dz_lesson_5/Test.java
@@ -0,0 +1,7 @@
+package dz_lesson_5;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(Factorial.factorial(4));
+    }
+}
