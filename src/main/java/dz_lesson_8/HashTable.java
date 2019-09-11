584df1c81daefa2d85b2323efe9dcfb8aeeddef9
MaximTrofim <oklickk@yandex.ru>
2019-09-11 23:19:37 +0300

    добавил урок 8

---
+++ src/main/java/dz_lesson_8/HashTable.java
@@ -0,0 +1,26 @@
+package dz_lesson_8;
+
+
+import dz_Lesson_3.ICollection;
+
+public interface HashTable<K, V> extends ICollection {
+
+    boolean put(K key, V value);
+
+    V get(K key);
+
+    boolean contains(K key);
+
+    V remove(K key);
+
+    void display();
+
+
+    interface Entry<K, V> {
+        K getKey();
+        V getValue();
+
+        void setKey(K key);
+        void setValue(V value);
+    }
+}
