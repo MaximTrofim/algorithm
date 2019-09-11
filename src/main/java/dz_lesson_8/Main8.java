3c503413be4de0a91a152596d664668a43d8160d
MaximTrofim <oklickk@yandex.ru>
2019-09-12 00:36:11 +0300

    тест

--- src/main/java/dz_lesson_8/Main8.java
+++ src/main/java/dz_lesson_8/Main8.java
@@ -7,11 +7,11 @@ public class Main8 {
         HashTable<ItemDefinition, Integer> hashTable = new HashTableImpl<>(5);
         //HashTable<ItemDefinition, Integer> hashTable = new DoubleHashTableImpl<>(5);
 
-        ItemDefinition orange = new ItemDefinition(85, "Orange");
-        ItemDefinition banana = new ItemDefinition(71, "Banana");
-        ItemDefinition lemon = new ItemDefinition(60, "Lemon");
-        ItemDefinition milk = new ItemDefinition(52, "Milk");
-        ItemDefinition potato = new ItemDefinition(80, "Potato");
+        ItemDefinition orange = new ItemDefinition(2, "Orange");
+        ItemDefinition banana = new ItemDefinition(10, "Banana");
+        ItemDefinition lemon = new ItemDefinition(3, "Lemon");
+        ItemDefinition milk = new ItemDefinition(4, "Milk");
+        ItemDefinition potato = new ItemDefinition(5, "Potato");
 
         hashTable.put(orange, 150);
         hashTable.put(banana, 100);
